089f206db2fc7b54f80b12509afa072722d61f2d
olakunle-remi <abbeypentious@gmail.com>
2023-05-23 10:10:35 -0700

    simple-shell collborated project main.h and _exits.c update

--- main.h
+++ main.h
@@ -8,21 +8,21 @@
 #include <sys/wait.h>
 
 #define DELIM " \a\n\r\t\""
-extern char **environment;
+extern char **environ;
 
 void from_terminal(void);
 void not_from_terminal(void);
+typedef int execve_args(char **args);
 
-char _getline(void);
+char *_getline(void);
 char **_strtok(char *strings);
-int execute_args(char **args);
 
 int _process(char **args);
 
 char *stream_reader(void);
 
 int _cd(char **args);
-int _exit(char **args);
+int _exitf(char **args);
 int _env(char **args);
 int _help(char **args);
 
